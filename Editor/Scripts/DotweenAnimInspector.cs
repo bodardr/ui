3d71d88d722f64513a9e725137fc901028944eae
Roman B <romain.bodard@videotron.ca>
2021-08-02 10:37:42 -0400

    Initial Commit

---
+++ Editor/Scripts/DotweenAnimInspector.cs
@@ -0,0 +1,108 @@
+﻿using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Bodardr.UI
+{
+    [CustomPropertyDrawer(typeof(DotweenAnim))]
+    public class DotweenAnimInspector : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return base.GetPropertyHeight(property, label) * 2 + 2;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, null, property);
+
+            var labelWidth = EditorGUIUtility.labelWidth;
+            EditorGUIUtility.labelWidth = 100;
+
+            position = EditorGUI.PrefixLabel(position, label);
+            EditorGUIUtility.labelWidth = labelWidth;
+
+            position.height = 18;
+
+            var totalWidth = position.width;
+            var baseX = position.x;
+
+            var indentLevel = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            var transitionRect = position;
+            transitionRect.width = totalWidth - 240;
+            position.x += transitionRect.width + 5;
+
+            var valueRect = position;
+            valueRect.width = 180;
+            position.x += valueRect.width - 35;
+
+            var timeRect = position;
+            timeRect.width = 90;
+            position.x += timeRect.width + 10;
+
+            position.x = baseX;
+            position.y += 18;
+
+            var easeRect = position;
+            easeRect.width = (totalWidth - 85) / 2;
+            position.x += easeRect.width + 5;
+
+            var loopsProperty = property.FindPropertyRelative(nameof(DotweenAnim.loops));
+            var loopsRect = position;
+
+            if (Mathf.Approximately(loopsProperty.intValue, 0))
+                loopsRect.width = easeRect.width + 75;
+            else
+                loopsRect.width = 70;
+
+            position.x += loopsRect.width + 10;
+
+            var loopsTypeRect = position;
+            loopsTypeRect.width = easeRect.width;
+
+            EditorGUI.PropertyField(transitionRect,
+                property.FindPropertyRelative(nameof(DotweenAnim.transformationType)),
+                GUIContent.none);
+
+            EditorGUIUtility.labelWidth = 35;
+            EditorGUI.PropertyField(valueRect, property.FindPropertyRelative(nameof(DotweenAnim.value)),
+                new GUIContent("Value:") {tooltip = "Values from and to. X : FROM, Y : TO"});
+
+            EditorGUIUtility.labelWidth = 55;
+            EditorGUI.PropertyField(timeRect, property.FindPropertyRelative(nameof(DotweenAnim.duration)),
+                new GUIContent("Time (s):"));
+
+            EditorGUI.PropertyField(easeRect, property.FindPropertyRelative(nameof(DotweenAnim.ease)), GUIContent.none);
+            EditorGUIUtility.labelWidth = labelWidth;
+
+            EditorGUIUtility.labelWidth = 40;
+            EditorGUI.PropertyField(loopsRect, loopsProperty);
+
+            if (!Mathf.Approximately(loopsProperty.intValue, 0))
+                EditorGUI.PropertyField(loopsTypeRect, property.FindPropertyRelative(nameof(DotweenAnim.loopType)),
+                    GUIContent.none);
+
+            EditorGUI.indentLevel = indentLevel;
+            EditorGUI.EndProperty();
+        }
+
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            var container = new VisualElement();
+
+            var transformationType =
+                new PropertyField(property.FindPropertyRelative(nameof(DotweenAnim.transformationType)));
+            var duration = new PropertyField(property.FindPropertyRelative(nameof(DotweenAnim.duration)));
+            var ease = new PropertyField(property.FindPropertyRelative(nameof(DotweenAnim.ease)));
+
+            container.Add(transformationType);
+            container.Add(duration);
+            container.Add(ease);
+
+            return container;
+        }
+    }
+}
---
+++ Runtime/Scripts/Data/DotweenAnim.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using DG.Tweening;
+using DG.Tweening.Core;
+using UnityEngine;
+
+namespace Bodardr.UI
+{
+    [Serializable]
+    public class DotweenAnim
+    {
+        public TransformationType transformationType;
+
+        public Vector2 value;
+
+        public float duration;
+
+        public Ease ease;
+
+        public int loops = 0;
+        public LoopType loopType;
+        private readonly bool isReversed = false;
+
+        public DotweenAnim()
+        {
+        }
+
+        public DotweenAnim(DotweenAnim copyFrom, bool reverse = false) : this()
+        {
+            duration = copyFrom.duration;
+            ease = copyFrom.ease;
+            value = copyFrom.value;
+            transformationType = copyFrom.transformationType;
+            isReversed = reverse ? !copyFrom.isReversed : copyFrom.isReversed;
+            loops = copyFrom.loops;
+            loopType = copyFrom.loopType;
+        }
+
+        public static DotweenAnim operator -(DotweenAnim a) => new DotweenAnim(a, true);
+
+        public Tween GetTweenFrom(Transform transform, CanvasGroup canvasGroup,
+            TweenAnimType tweenType = TweenAnimType.Additive)
+        {
+            Tween tween;
+
+            switch (transformationType)
+            {
+                case TransformationType.Scale:
+                    var a = transform.DOScale(isReversed ? value.x : value.y, duration)
+                        .From(isReversed ? value.y : value.x);
+
+                    if (tweenType == TweenAnimType.Override && loops == 0)
+                        a.NoFrom();
+
+                    tween = a;
+                    break;
+
+                case TransformationType.Fade:
+                    var b = canvasGroup.DOFade(isReversed ? value.x : value.y, duration)
+                        .From(isReversed ? value.y : value.x);
+
+                    if (tweenType == TweenAnimType.Override && loops == 0)
+                        b.NoFrom();
+
+                    tween = b;
+                    break;
+
+                default:
+                    var c = canvasGroup.DOFade(isReversed ? value.x : value.y, duration)
+                        .From(isReversed ? value.y : value.x);
+
+                    if (tweenType == TweenAnimType.Override && loops == 0)
+                        c.NoFrom();
+
+                    tween = c;
+                    break;
+            }
+
+            tween.SetEase(ease);
+
+            if (loops != 0)
+                tween.SetLoops(loops, loopType);
+
+            return tween;
+        }
+    }
+
+    public enum TweenAnimType
+    {
+        Additive,
+        Override
+    }
+
+    public enum TransformationType
+    {
+        Scale,
+        Fade,
+    }
+}
