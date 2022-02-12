9acce47efd1aad8b86bd0781e952e7296461ee25
Romain Bodard <romain.bodard@videotron.ca>
2022-02-12 15:50:29 -0500

    Addressed Bug report 11

---
+++ Runtime/Scripts/AudioSourceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public static class AudioSourceExtensions
+{
+    public static void Crossfade(this AudioSource a, AudioSource b, float duration)
+    {
+        if (!b.isPlaying)
+            b.Play();
+
+        b.DOFade(1, duration).From(0);
+        a.DOFade(0, duration).From(1).OnComplete(a.Stop);
+    }
+}
