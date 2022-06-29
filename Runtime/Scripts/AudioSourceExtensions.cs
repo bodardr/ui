42e0e5fe61739489b2178dc4c942386bb280ef93
Romain Bodard <romain.bodard@videotron.ca>
2022-06-29 15:25:00 -0400

    WIP : Enhanced Attacking System to prevent movement during attacks and ability animations.
    Implemented Hero VOs and sounds related to monster placement, level up and victory / defeat.

--- Runtime/Scripts/AudioSourceExtensions.cs
+++ Runtime/Scripts/AudioSourceExtensions.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 
 public static class AudioSourceExtensions
 {
-    public static void Crossfade(this AudioSource a, AudioSource b, float duration = 1)
+    public static void CrossfadeTo(this AudioSource origin, AudioSource destination, float duration = 1)
     {
-        if (!b.isPlaying)
-            b.Play();
+        if (!destination.isPlaying)
+            destination.Play();
 
-        b.DOFade(1, duration).From(0);
-        a.DOFade(0, duration).From(1).OnComplete(a.Stop);
+        destination.DOFade(1, duration).From(0);
+        origin.DOFade(0, duration).From(1).OnComplete(origin.Stop);
     }
 }
