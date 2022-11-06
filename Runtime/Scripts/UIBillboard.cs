a5a4d519ecbaf59810612b3a24e03f56b63583bd
Romain Bodard <romain.bodard@videotron.ca>
2022-11-06 00:58:34 -0400

    UI Billboard : Aligns to camera

---
+++ Runtime/Scripts/UIBillboard.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class UIBillboard : MonoBehaviour
+{
+    private static Transform cam;
+
+    [SerializeField]
+    private UpdateType updateType;
+
+    private void Awake()
+    {
+        if (!cam && Camera.main)
+            cam = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        if (updateType != UpdateType.Normal)
+            return;
+
+        UpdateBillboard();
+    }
+
+    private void FixedUpdate()
+    {
+        if (updateType != UpdateType.Fixed)
+            return;
+
+        UpdateBillboard();
+    }
+
+    private void LateUpdate()
+    {
+        if (updateType != UpdateType.Late)
+            return;
+
+        UpdateBillboard();
+    }
+
+    private void UpdateBillboard()
+    {
+        transform.rotation = cam!.rotation;
+    }
+}
